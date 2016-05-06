8dea7d1eaccff3098eedb4499e15082bedb397db
Jingfan Sun <sunjfsjfan@gmail.com>
2016-05-06 17:03:16 -0400

    Add HelloWorld.cpp

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -8,8 +8,8 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    std::cout << "Hello, World! Jingfan!" << std::endl;
     return 0;
 }
